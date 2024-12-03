2dee63756ae7c4a491b57a91a5291b3c33892755
Paulo Bahia <>
2024-12-03 17:38:45 -0300

    chore: Adicionar variáveis de ambiente e dependências.
    - Adicionar variáveis de ambiente para API_URL e API_KEY.
    - Adicione chaves de API e URLs para os serviços Audie, Vagalume e Discogs no arquivo `.env.example`.
    - Atualizar nome do aplicativo para "Audie Template".
    - Adicionar configuração para carregar variáveis de ambiente do arquivo .env usando o react-native-dotenv.
    - Adicione variáveis de ambiente para chaves de API necessárias.
    - Adiciona dependência "react-native-dotenv" com a versão 3.4.11.
    - Adicionar a dependência `react-native-dotenv` versão 3.4.11.
    - Atualiza a dependência do pacote "react-native-dotenv" para a versão 3.4.11 e adiciona a dependência "dotenv" na versão 16.4.5.

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,15 @@ module.exports = {
   plugins: [
     'nativewind/babel',
     'react-native-reanimated/plugin',
+    'module:react-native-dotenv',
+    {
+      moduleName: '@env',
+      path: '.env',
+      blacklist: null,
+      whitelist: null,
+      safe: false,
+      allowUndefined: true,
+    },
     [
       'module-resolver',
       {
---
+++ env.d.ts
@@ -0,0 +1,7 @@
+declare module '@env' {
+  export const AUDIE_API_URL: string;
+  export const AUDIE_API_KEY: string;
+  export const VAGALUME_API_KEY: string;
+  export const DISCOGS_KEY: string;
+  export const DISCOGS_SECRET: string;
+}
